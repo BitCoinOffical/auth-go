133c92c483cbf8e66105c4e10d809fab0fdcf14d
BitCoinOffical <jlucehok228@gmail.com>
2026-07-01 21:01:38 +0500

    fix: migration dir

--- Session-based/internal/adapters/migrations/migrations.go
+++ Session-based/internal/adapters/migrations/migrations.go
@@ -9,7 +9,11 @@ import (
 	"github.com/pressly/goose/v3"
 )
 
-func RunMigrations(pool *pgxpool.Pool, migrationsDir string) error {
+const (
+	migrationsDir = "../../../migrations"
+)
+
+func RunMigrations(pool *pgxpool.Pool) error {
 	db := stdlib.OpenDBFromPool(pool)
 
 	if err := goose.SetDialect("postgres"); err != nil {
@@ -22,7 +26,7 @@ func RunMigrations(pool *pgxpool.Pool, migrationsDir string) error {
 	return nil
 }
 
-func RollbackLast(ctx context.Context, db *sql.DB, migrationsDir string) error {
+func RollbackLast(ctx context.Context, db *sql.DB) error {
 	if err := goose.DownContext(ctx, db, migrationsDir); err != nil {
 		return err
 	}
